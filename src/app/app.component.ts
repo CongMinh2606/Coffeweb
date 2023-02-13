adf82302ec35930d307e396bdf9dab6b5ce29831
Cong Minh <minh.tc03208@sinhvien.hoasen.edu.vn>
2023-02-13 21:48:20 +0700

    webcoffe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DemoDog';
+  title = 'CM Coffe';
+  Coffe:Array<any> = [
+    {
+      id: 1,
+      name: "Cà phê đá",
+      price: "20.000 VND",
+      image:"../assets/cfda.webp",
+      weight: "1/ly ",
+      describe: "Ngọt/Đắng/Đậm vị cà phê",
+      oder: false,
+    },
+    {
+      id: 2,
+      name: "Cà phê sữa đá",
+      price:"20.000 VND",
+      image:"../assets/caphesua.webp",
+      weight: "1/ly ",
+      describe: "Ngọt/Đắng/Đậm vị cà phê",
+      oder: false,
+    },
+    {
+      id: 3,
+      name: "Bạc Xỉu Nóng",
+      price: "10.000 VND",
+      image:"../assets/bacxiunong.webp",
+      weight: "1/ly ",
+      describe: "Ngọt/Đắng/Đậm vị cà phê",
+      oder: false,
+    },
+    {
+      id: 4,
+      name: "Đen Nóng",
+      price: "10.000 VND",
+      image:"../assets/dennong.webp",
+      weight: "1/ly ",
+      describe: "Ngọt/Đắng/Đậm vị cà phê",
+      oder: false,
+    },
+    {
+      id: 5,
+      name: "Espresso Nóng",
+      price: "10.000 VND",
+      image:"../assets/espressonong.webp",
+      weight: "1/ly ",
+      describe: "Ngọt/Đắng/Đậm vị cà phê",
+      oder: false,
+    },
+    {
+      id: 6,
+      name: "Caramelmacchiato Nóng",
+      price: "10.000 VND",
+      image:"../assets/caramelmacchiato.webp",
+      weight: "1/ly ",
+      describe: "Ngọt/Đắng/Đậm vị cà phê",
+      oder: false,
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatInputModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
